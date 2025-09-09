ccfb2d3dbc0026aaeb3681b7e54fedba8a300862
brdfe <brdfe@protonmail.com>
2025-09-10 01:00:56 +1000

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('node:http');
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.flushHeaders();
+
+  const interval = setInterval(() => {
+    res.write(`data: ${new Date().toISOString()}\n\n`);
+  }, 1000);
+
+  // 👇 Detect client disconnect
+  req.on("close", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+    res.end();
+  });
+
+  res.on("close", () => {
+    console.log('couldnt contact client')
+  })
+});
+
+server.listen(3000, 'localhost', () => {
+  console.log('Server started on port 3000');
+})
